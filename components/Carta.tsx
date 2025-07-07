a99c43b38aaf3dc24e16199b29cf9d8602113285
Douglas Moura <douglas12345.dsm@gmail.com>
2025-07-07 11:40:54 -0300

    style: Correção estrutura/estilo componente Carta.tsx
    - Para evitar problemas de layout com o elemento baralho o componente Carta.tsx foi ajustado para que ImageBackground fique dentro de uma View assim como as outras cartas

--- components/Carta.tsx
+++ components/Carta.tsx
@@ -9,11 +9,13 @@ export default function Carta({ carta }: Props) {
     if (!carta) {
         // Mostra o verso da carta
         return (
-            <ImageBackground
-                source={require('../assets/img/card-back.jpg')}
-                style={styles.cartaContainer}
-                imageStyle={{ borderRadius: 6 }}
-            />
+            <View style={styles.cartaContainer}>
+                <ImageBackground
+                    source={require('../assets/img/card-back.jpg')}
+                    style={styles.imgVersoBaralho}
+                    imageStyle={{ borderRadius: 6 }}
+                />
+            </View>
         )
     }
 
@@ -30,15 +32,21 @@ const styles = StyleSheet.create({
     cartaContainer: {
         width: 50,
         height: 70,
-        borderWidth: 1,
-        borderColor: "#ccc",
+        borderWidth: 2,
+        borderColor: "#fff",
         borderRadius: 6,
-        padding: 8,
-        backgroundColor: "#fff",
+        //padding: 8,
+        backgroundColor: "#ededed",
         margin: 4,
     },
+    imgVersoBaralho: {
+        width: '100%',
+        height: '100%',
+    },
     cartaNumero: {
+        margin: 8,
         fontSize: 12,
+        fontWeight: 'bold',
         color: "#333",
     },
     cartaSimbolo: {
